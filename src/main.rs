65668de195a74eb02ad21d800fc84f965461848b
Sumeet Agarwal <sumeet.a@gmail.com>
2021-12-06 21:02:38 -0800

    set up to read from a file properly

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod interp;
 mod parser;
 
 fn main() -> anyhow::Result<()> {
-    let program_str = std::fs::read_to_string("./scratch.roni")?;
+    let filename = std::env::args()
+        .nth(1)
+        .ok_or(anyhow::anyhow!("no filename given"))?;
+
+    let program_str = std::fs::read_to_string(filename)?;
     let program = parser::parse_program(&program_str)?;
     let mut interp = interp::Interpreter::new();
     for statement in &program.statements {
